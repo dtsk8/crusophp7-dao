b80c0944d54aa0b3b76b7a90443bd1623062a39e
Danilo Turqueti <danilo.turquet@gmail.com>
2018-02-02 18:04:55 -0200

    classe usuario

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
 require_once("config.php");
-
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+echo "<br/>";
+*/
+$root = new Usuario();
+
+$root->loadbyId(4);
+
+echo $root;
+
+echo "<br/>";
 
 ?>
